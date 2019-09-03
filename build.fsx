94537eb56540204dc67f5a440fd1565c739b6439
Johan Oelrich <johan.oelrich@vesicode.com>
2019-09-03 15:46:02 +0200

    Some tools for cleaning

---
+++ build.fsx
@@ -0,0 +1,30 @@
+#r "paket:
+nuget Fake.IO.FileSystem
+nuget Fake.Core.Target //"
+#load "./.fake/build.fsx/intellisense.fsx"
+
+open Fake.Core
+open Fake.IO
+
+let removableDirs = [
+  "./_next"
+  "./about"
+  "./news"
+  "./privacy-policy"
+  "./products"
+  "./static"
+  "./technology"
+]
+
+Target.create "Clean" (fun _ ->
+  Shell.deleteDirs removableDirs)
+
+Target.create "Default" (fun _ ->
+  Trace.trace "Cleaning out the old")
+
+open Fake.Core.TargetOperators
+
+"Clean"
+  ==> "Default"
+
+Target.runOrDefault "Default"
